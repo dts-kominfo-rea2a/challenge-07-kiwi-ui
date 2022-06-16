b71ce496a5881055ec682b92434610724c6734c3
kiwi-ui <yusnianggara@gmail.com>
2022-06-16 14:36:22 +0700

    .

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (param1) => {
+  let bioskop1 = await promiseTheaterIXX();
+  let bioskop2 = await promiseTheaterVGC();
+
+  let cinemas = bioskop1.concat(bioskop2);
+  let results = 0;
+
+  cinemas.map((bioskop1) => bioskop1.hasil === param1 && (results += 1));
+
+  return results;
+};
 
 module.exports = {
   promiseOutput,
